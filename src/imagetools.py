6405ae8b1879716152e498c05261329f726ea12e
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2023-01-26 23:16:12 +0100

    refactored resources

--- src/imagetools.py
+++ src/imagetools.py
@@ -1,24 +1,3 @@
-#!/usr/bin/python
-############################################################################
-#    Copyright (C) 2013 by Jorrit Vander Mynsbrugge                        #
-#    jorrit.vm@gmail.com                                                   #
-#                                                                          #
-#    This program is free software; you can redistribute it and#or modify  #
-#    it under the terms of the GNU General Public License as published by  #
-#    the Free Software Foundation; either version 2 of the License, or     #
-#    (at your option) any later version.                                   #
-#                                                                          #
-#    This program is distributed in the hope that it will be useful,       #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-#    GNU General Public License for more details.                          #
-#                                                                          #
-#    You should have received a copy of the GNU General Public License     #
-#    along with this program; if not, write to the                         #
-#    Free Software Foundation, Inc.,                                       #
-#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
-############################################################################
-
 #python imports
 import os
 import sys
@@ -51,7 +30,7 @@ from imageoperations.Judge import *
 class MainWindow(QMainWindow):
     
     def __init__(self, parent = None):
-        super(MainWindow,self).__init__(parent)
+        super(MainWindow, self).__init__(parent)
        
         self.settings = self.getSettings()
         self.supervisor = Supervisor(self.settings['nThreads'], self)
