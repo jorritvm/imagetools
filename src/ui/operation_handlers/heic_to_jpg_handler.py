f05cbe08b151cb8a5fb6c39130abf448a6f4c719
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2025-08-19 08:18:17 +0200

    making heic2jpg handler multithreaded to avoid blocking the UI thread

--- src/ui/operation_handlers/heic_to_jpg_handler.py
+++ src/ui/operation_handlers/heic_to_jpg_handler.py
@@ -1,3 +1,4 @@
+from PyQt6.QtCore import QObject, pyqtSignal, QThread
 from PyQt6.QtWidgets import QDialog, QFileDialog
 
 from operations.heic_to_jpg import heic_to_jpg_operation
@@ -35,16 +36,47 @@ class HeicToJpegDialog(QDialog, Ui_heic_to_jpg):
         self.btn_close_redirect.clicked.connect(self.on_close_redirect)
 
     def start_operation(self):
-        def callback(message, progress):
-            self.progress_bar.setValue(progress)
-            self.text_output.append(message)
-
         # fetch the input and clean the dialog log textedit
         folder_path = self.edit_folder_path.text()
         self.text_output.clear()
-        # call the takeout operation
-        return heic_to_jpg_operation(folder_path, callback)
+
+        # create worker + thread
+        self.worker_thread = QThread()
+        self.worker = HeicToJpegtWorker(folder_path)
+        self.worker.moveToThread(self.worker_thread)
+
+        # connect signals
+        self.worker.progress.connect(self.on_progress)
+        self.worker_thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.worker_thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.worker_thread.finished.connect(self.worker_thread.deleteLater)
+
+        # start background thread
+        self.worker_thread.start()
+
+    def on_progress(self, message, progress):
+        self.progress_bar.setValue(progress)
+        self.text_output.append(message)
 
     def on_close_redirect(self):
         self.go_to_output = True
         self.accept()
+
+
+class HeicToJpegtWorker(QObject):
+    """ Worker for the takeout operation to keep UI responsive."""
+    progress = pyqtSignal(str, int)  # message, progress
+    finished = pyqtSignal()
+
+    def __init__(self, folder_path):
+        super().__init__()
+        self.folder_path = folder_path
+
+    def run(self):
+        def callback(message, progress):
+            self.progress.emit(message, progress)
+
+        # run your heavy function in this thread
+        heic_to_jpg_operation(self.folder_path, callback)
+        self.finished.emit()
